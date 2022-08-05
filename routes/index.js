225f4820e534b9f3f7d790b0a09ef2081f125de4
21wh1a05g5 <21wh1a05g5@bvrithyderabad.edu.in>
2022-08-05 11:06:33 +0530

    changed views

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Harshitha' });
+  res.render('index', { title: 'Hello world 2022' });
 });
 
 module.exports = router;
