a20dc5dda73dd0274f4008b4f4377cedb2e06492
21wh1a05g5 <21wh1a05g5@bvrithyderabad.edu.in>
2022-08-09 17:27:43 +0530

    create

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
-
+const path = require("path")
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Helloworld2022' });
+router.get('/', function (req, res, next) {
+  res.sendFile('index.html', {
+    root: path.join(__dirname, '../views')
+  })
 });
 
 module.exports = router;
